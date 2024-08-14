5914d1bb8ee2093779d6e05d3abc3d4715b90f70
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-08-14 09:46:55 +0530

    class meta created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class BaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created_at= models.DateField(auto_created=True)
+    updated_at= models.DateField(auto_created=True)
+    # to treat this class as class and not as a model write the below code...
+    class Meta:
+        abstract=True
+    
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_slug = models.SlugField(unique=True)
+    product_description = models.TextField(unique=True)
+    product_price = models.IntegerField(default=0)
+class ProductImages(models.Model):
+    product_image = models.CharField(max_length=100)
+class DummyModel(models.Model):
+    pass
